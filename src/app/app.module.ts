04f333e39f4bf8bfc7ae924db93f77d200c13fbc
Matt Mckeller <msmckeller@realequitymanagement.com>
2017-09-12 10:12:53 -0500

    Add app routing, move placeholder body into home component. Handle sidenav through service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule }    from '@angular/http';
+import { HttpModule } from '@angular/http';
 
+import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from './app.component';
+
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MdButtonModule, MdCheckboxModule, MdToolbarModule, MdIconModule, MdSidenavModule} from '@angular/material';
+
 import { MainNavbarComponent } from './main-navbar/main-navbar.component';
 import { MainFooterComponent } from './main-footer/main-footer.component';
 import { SideNavService } from './side-nav.service';
+import { HomeComponent } from './home/home.component';
 
 const AngularMaterialDependencies = [
   MdButtonModule,
@@ -23,11 +27,13 @@ const AngularMaterialDependencies = [
   declarations: [
     AppComponent,
     MainNavbarComponent,
-    MainFooterComponent
+    MainFooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
+    AppRoutingModule,
     FormsModule,
     BrowserAnimationsModule,
     ...AngularMaterialDependencies
