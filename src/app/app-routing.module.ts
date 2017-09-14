250c2d6b0762ffea682e1b7cf4e51c2f20f5d42a
Matt Mckeller <mattmckeller@gmail.com>
2017-09-14 13:42:22 -0500

    Add product path with parameter of id

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { VintageComponent } from './vintage/vintage.component';
+import { ProductComponent } from './store/product/product.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home',  component: HomeComponent },
   { path: 'about',  component: AboutComponent },
-  { path: 'vintage',  component: VintageComponent }
+  { path: 'vintage',  component: VintageComponent },
+  { path: 'vintage/:id', component: ProductComponent },
 ];
 
 @NgModule({
