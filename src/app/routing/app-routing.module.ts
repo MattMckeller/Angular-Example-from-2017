0df2d9c8096e76e9b77ccb39a7de94601667a2c5
Matt Mckeller <mattmckeller@gmail.com>
2017-09-14 10:32:52 -0500

    Update page references

--- src/app/routing/app-routing.module.ts
+++ src/app/routing/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
+import { HomeComponent } from '../home/home.component';
+import { AboutComponent } from '../about/about.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
