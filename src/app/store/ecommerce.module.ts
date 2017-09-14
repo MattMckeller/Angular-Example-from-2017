50b0c673b4a40841b7ab37eed81f469d0482523b
Matt Mckeller <mattmckeller@gmail.com>
2017-09-14 10:57:04 -0500

    Setup ecommerce module to instantiate existing services and components

--- src/app/store/ecommerce.module.ts
+++ src/app/store/ecommerce.module.ts
@@ -1,12 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { ProductService } from './product.service';
+import { CartService } from './cart.service';
+
+import { ProductComponent } from './product/product.component';
 import { CartComponent } from './cart/cart.component';
 import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
-  imports: [
-    CommonModule
+  imports: [],
+  exports: [
+    ProductComponent,
+    CartComponent,
+    ProductListComponent
   ],
-  declarations: [CartComponent, ProductListComponent]
+  declarations: [
+    ProductComponent,
+    CartComponent,
+    ProductListComponent
+  ],
+  providers: [
+    ProductService,
+    CartService
+  ],
 })
 export class EcommerceModule { }
