be50f51c5c4c52fc2a8da34d449fd30a9fb4ea77
Matt Mckeller <mattmckeller@gmail.com>
2017-09-14 16:08:22 -0500

    Clean product component class. On init this component now subscribes to the route and updates the product based off changing route params

--- src/app/store/product/product.component.ts
+++ src/app/store/product/product.component.ts
@@ -1,47 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 import { ProductService } from '../product.service';
 import { CartService } from '../cart.service';
+import { Product } from '../product';
 
+import 'rxjs/add/operator/switchMap';
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
-  /**
-   * Id of product
-   */
-  id: string;
-
-  /**
-   * Product data
-   */
-  product;
-
-  /**
-   * Product attributes
-   */
-  attributes = [];
-
-  /**
-   * Product tags
-   */
-  tags = [];
+  product: Product;
 
   constructor(
     private productService: ProductService,
-    private cart: CartService
+    private cart: CartService,
+    private route: ActivatedRoute
   ) {}
 
   addToCart(){
-    console.log(`Add {this.product} to cart!`);
+    console.log('Add '+this.product+' to cart!');
   }
 
-  buyNow(){
-    console.log(`Buy {this.product} now!`);
+  buy(){
+    console.log('Checkout '+this.product+'now!');
   }
 
   ngOnInit() {
+    // this.product = this.productService.getById(this.route.snapshot.params['id']);
+    this.route.params.subscribe(params =>
+      this.product = this.productService.getById(params['id'])
+    );
+    //Ex for when observables are implemented
+    // this.route.paramMap
+    //   .switchMap((params: ParamMap) => this.heroService.getHero(+params.get('id')))
+    //   .subscribe(hero => this.hero = hero);
   }
 
+  formattedPrice(){
+    return this.productService.formatPrice(this.product.price);
+  }
 }
