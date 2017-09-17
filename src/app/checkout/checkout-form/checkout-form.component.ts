640917303cde3e06392963fe909464ca84131801
Matt Mckeller <mattmckeller@gmail.com>
2017-09-17 14:09:16 -0500

    First section of checkout form created. Shipping information collected. Basic required field validation is displayed.

---
+++ src/app/checkout/checkout-form/checkout-form.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit } from '@angular/core';
+import { CheckoutModel } from '../checkout-model';
+import {FormGroup, FormControl, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'app-checkout-form',
+  templateUrl: './checkout-form.component.html',
+  styleUrls: ['./checkout-form.component.css']
+})
+export class CheckoutFormComponent implements OnInit {
+
+  submitted = false;
+
+  model: CheckoutModel;
+
+  checkoutForm: FormGroup;
+
+  onSubmit() { this.submitted = true; console.log('form was submit!') }
+
+  constructor() {}
+
+  ngOnInit() {
+    this.model = new CheckoutModel();
+
+    this.checkoutForm = new FormGroup({
+      'firstName': new FormControl(this.model.firstName, [
+        Validators.required
+      ]),
+      'lastName': new FormControl(this.model.lastName, [
+        Validators.required
+      ]),
+      'address1': new FormControl(this.model.address1, [
+        Validators.required
+      ]),
+      'address2': new FormControl(this.model.address2, [
+        Validators.required
+      ]),
+      'city': new FormControl(this.model.city, [
+        Validators.required
+      ]),
+      'state': new FormControl(this.model.state, [
+        Validators.required
+      ]),
+      'zip': new FormControl(this.model.zip, [
+        Validators.required
+      ]),
+      'phone': new FormControl(this.model.phone, [
+        Validators.required
+      ])
+    });
+  }
+
+
+  get firstName() { return this.checkoutForm.get('firstName'); }
+  get lastName() { return this.checkoutForm.get('lastName'); }
+  get address1() { return this.checkoutForm.get('address1'); }
+  get address2() { return this.checkoutForm.get('address2'); }
+  get city() { return this.checkoutForm.get('city'); }
+  get state() { return this.checkoutForm.get('state'); }
+  get zip() { return this.checkoutForm.get('zip'); }
+  get phone() { return this.checkoutForm.get('phone'); }
+
+}
---
+++ src/app/checkout/checkout-model.ts
@@ -0,0 +1,13 @@
+export class CheckoutModel {
+  constructor(
+    public firstName: string = null,
+    public lastName: string = null,
+    public address1: string = null,
+    public address2: string = null,
+    public city: string = null,
+    public state: string = null,
+    public zip: string = null,
+    public phone: string = null
+
+  ) {  }
+}
