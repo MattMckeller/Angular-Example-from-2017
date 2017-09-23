f40a541efe048f17c642bd2b9285ace2e65d2eb2
Matt Mckeller <mattmckeller@gmail.com>
2017-09-23 12:38:09 -0500

    Add getQuantity method, findExact method, comments, and a few bug fixes

--- src/app/store/cart.service.ts
+++ src/app/store/cart.service.ts
@@ -14,21 +14,41 @@ export class CartService {
     } as Cart;
   }
 
+  /**
+   * Adds 1 product to the cart
+   * @param product
+   */
   add(product: Product){
     console.log('attempt to add product');
     this.cart.items.push(product);
   }
 
+  /**
+   * Removes 1 of the specified items from the cart.
+   * @param product
+   */
   remove(product: Product){
-    this.cart.items = [];
-
     console.log('Attempt to remove product');
+    let index = this.findExact(product).indexOf(product);
+    if(index !== -1){
+      this.cart.items.splice(index,1);
+      return true;
+    }else{
+      return false;
+    }
   }
 
+  /**
+   * Empties the users cart
+   */
   empty(){
     this.cart.items.forEach((item)=> this.remove(item));
   }
 
+  /**
+   * Get all products in the cart.
+   * @return {Cart}
+   */
   get(): Cart{
     console.log('Attempt to retrieve cart');
     //todo create a real cart object somewhere and have a computed property...
@@ -37,5 +57,23 @@ export class CartService {
     this.cart.items.forEach((item)=>(total += item.price));
     this.cart.itemsTotal = total;
     return this.cart;
+  }
+
+  /**
+   * Finds exact matches of the provided product in the cart and returns them
+   * @param product
+   * @return {Product[]}
+   */
+  findExact(product: Product): Array<Product>{
+    return this.get().items.filter(item => product.id == item.id);
+  }
+
+  /**
+   * Returns the current quantity of the provided item in the cart
+   * @param product Product to find
+   * @return {number}
+   */
+  getQuantity(product: Product): number{
+    return this.findExact(product).length;
   }
 }
