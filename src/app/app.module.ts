20526f2a7025c0ad093ca31f45cb8f18dba30168
Matt Mckeller <msmckeller@realequitymanagement.com>
2017-10-04 14:55:06 -0500

    Numerous updates mostly replicated from my expanse project. Updated sidenav and separated it out from app component, added custom material design and typography files ( to be edited ). Import flex layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { EcommerceModule } from '@store/ecommerce.module';
 
 import { MainNavbarComponent } from './main-navbar/main-navbar.component';
 import { MainFooterComponent } from './main-footer/main-footer.component';
+import { SidenavComponent } from "@app/sidenav/sidenav.component";
 import { SideNavService } from './side-nav.service';
 import { HomeComponent } from '@pages/home/home.component';
 import { AboutComponent } from '@pages/about/about.component';
@@ -18,12 +19,15 @@ import { CheckoutComponent } from '@pages/checkout/checkout.component';
 import { CheckoutFormComponent } from '@forms/checkout-form/checkout-form.component';
 import { AddressFieldsComponent } from '@forms/input-groups/address-fields/address-fields.component';
 import { PaymentFieldsComponent } from '@forms/input-groups/payment-fields/payment-fields.component';
+import {HttpClientModule} from "@angular/common/http";
+import {FlexLayoutModule} from "@angular/flex-layout";
 
 @NgModule({
   declarations: [
     AppComponent,
     MainNavbarComponent,
     MainFooterComponent,
+    SidenavComponent,
     HomeComponent,
     AboutComponent,
     VintageComponent,
@@ -34,11 +38,12 @@ import { PaymentFieldsComponent } from '@forms/input-groups/payment-fields/payme
   ],
   imports: [
     BrowserModule,
-    HttpModule,
+    HttpClientModule,
     AppRoutingModule,
     MaterialModule,
     FormsModule,
     ReactiveFormsModule,
+    FlexLayoutModule,
     EcommerceModule
   ],
   providers: [
---
+++ src/app/sidenav/sidenav.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
+import {SideNavService} from "@app/side-nav.service";
+
+@Component({
+  selector: 'app-sidenav',
+  templateUrl: './sidenav.component.html',
+  styleUrls: ['./sidenav.component.scss']
+})
+export class SidenavComponent implements OnInit {
+  tabs = [
+    {
+      link: '/home',
+      icon: 'home',
+      text: 'Home'
+    },
+    {
+      link: '/about',
+      icon: 'view_carousel',
+      text: 'About Us'
+    },
+    {
+      link: '/vintage',
+      icon: 'donut_large',
+      text: 'Vintage'
+    }
+  ];
+
+  constructor(
+    private router: Router,
+    private sideNavService: SideNavService
+  ) { }
+
+  ngOnInit() {
+  }
+
+  handleClose() {
+    this.sideNavService.close();
+  }
+}
