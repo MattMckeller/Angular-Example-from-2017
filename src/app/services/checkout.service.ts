ad5d91ddacd4f7987ea47707ce4a7734ec07a5a6
Matt Mckeller <msmckeller@realequitymanagement.com>
2017-10-09 12:33:50 -0500

    Add http post call to api server with checkout data

--- src/app/services/checkout.service.ts
+++ src/app/services/checkout.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
 import {CheckoutModel} from "@models/checkout";
+
+import 'rxjs/add/operator/retry';
 
 @Injectable()
 export class CheckoutService {
@@ -10,13 +12,31 @@ export class CheckoutService {
   ) { }
 
   submit(checkoutData: CheckoutModel) {
-    console.log('submit from service');
-    console.log(checkoutData);
+    let url = 'http://api.expanseservices.com/api/checkout/submit';
+    this.http.post(url, checkoutData)
+      .subscribe(
+        data => {
+          this.confirmSubmit();
+        },
+        (err: HttpErrorResponse) => {
+          if (err.error instanceof Error) {
+            // A client-side or network error occurred.
+            console.log('An error occurred:', err.error.message);
+          } else {
+            // The backend returned an unsuccessful response code.
+            console.log(`Backend returned code ${err.status}, body was: ${err.error}`);
+          }
+        }
+      );
   }
 
   save(checkoutData: CheckoutModel) {
     console.log('submit from service');
     console.log(checkoutData);
+  }
+
+  confirmSubmit() {
+    console.log('submit confirmed do something now?');
   }
 
 }
