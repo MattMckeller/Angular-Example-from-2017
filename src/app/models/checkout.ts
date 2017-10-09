56627287f86cde45eae3ff7ec85d33b2808564d5
Matt Mckeller <msmckeller@realequitymanagement.com>
2017-10-09 10:27:03 -0500

    Update shipping selection model field to be of type shipping option instead of string.

--- src/app/models/checkout.ts
+++ src/app/models/checkout.ts
@@ -1,11 +1,12 @@
 import { Address } from './address';
 import { CardPaymentMethod } from './card-payment-method';
+import {ShippingOption} from "@models/shipping-option";
 
 export class CheckoutModel {
   constructor(
     public shippingAddress: Address = new Address(),
     public phoneNumber: string = null,
-    public shippingSelection: string = null,
+    public shippingSelection: ShippingOption = null,
     public cardPaymentMethod: CardPaymentMethod = new CardPaymentMethod(),
     public useShippingAddressForBilling: Boolean = true,
     public billingAddress: Address = new Address()
