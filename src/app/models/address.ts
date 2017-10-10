557b74092a11c5149709a8dfc45ab6e289389e33
Matt Mckeller <msmckeller@realequitymanagement.com>
2017-10-10 10:07:13 -0500

    Default address fields to strings

--- src/app/models/address.ts
+++ src/app/models/address.ts
@@ -1,11 +1,11 @@
 export class Address {
   constructor(
-    public firstName: string = null,
-    public lastName: string = null,
-    public address1: string = null,
-    public address2: string = null,
-    public city: string = null,
-    public state: string = null,
-    public zip: string = null
+    public firstName: string = '',
+    public lastName: string = '',
+    public address1: string = '',
+    public address2: string = '',
+    public city: string = '',
+    public state: string = '',
+    public zip: string = ''
   ) {}
 }
