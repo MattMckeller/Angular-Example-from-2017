4a295de1e53ed84559e9877787c4ccc0c7555b91
Matt Mckeller <msmckeller@realequitymanagement.com>
2017-10-16 15:31:55 -0500

    Now retrieving image through api calls.

--- src/app/store/product/product.component.ts
+++ src/app/store/product/product.component.ts
@@ -3,9 +3,11 @@ import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { ProductService } from '../product.service';
 import { CartService } from '../cart.service';
 import { Product } from '../product';
+import _ from "lodash";
 
 import 'rxjs/add/operator/switchMap';
 import {Observable} from "rxjs/Observable";
+import {ProductImageService} from "@services/product-image.service";
 
 @Component({
   selector: 'app-product',
@@ -13,12 +15,15 @@ import {Observable} from "rxjs/Observable";
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
+  finishedLoading = false;
   productObservable: Observable<Product>;
   product: Product;
+  thumbnails: Array<any> = [];
   hasBeenAdded: boolean = false;
 
   constructor(
     private productService: ProductService,
+    private productImageService: ProductImageService,
     private cartService: CartService,
     private route: ActivatedRoute,
     private router: Router
@@ -55,10 +60,40 @@ export class ProductComponent implements OnInit {
     //Ex for when observables are implemented
     this.route.paramMap
       .switchMap((params: ParamMap) => this.productService.getById(+params.get('id')))
-      .subscribe(product => this.product = product);
+      .subscribe(product => {
+        this.product = product;
+        this.fetchThumbnails();
+      });
+  }
+
+  /**
+   * Retrieve images from api, and convert them to object urls. Stores urls in thumbnail variable.
+   */
+  fetchThumbnails(){
+    let _this = this;
+    let product = this.product;
+    _this.productImageService.getProductImage(product).then(
+      (image) => {
+        let imageUrl = URL.createObjectURL(image);
+        _this.thumbnails[product.id] = _this.thumbnails[product.id] || [];
+        _this.thumbnails[product.id].push(imageUrl);
+      });
+  }
+
+  /**
+   * Returns the thumbnail for the provided product
+   * @param product
+   * @return false | blobUrl
+   */
+  getThumbnail(product) {
+    if(product && this.thumbnails[product.id] && this.thumbnails[product.id][0]){
+      return this.thumbnails[product.id][0];
+    }else{
+      return false;
+    }
   }
 
   formattedPrice(){
-    return this.productService.formatPrice(this.product.price);
+    return this.productService.formatPrice(this.product);
   }
 }
