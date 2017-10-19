6b683c412fe42cbc7d319adb0edac801c827c659
Matt Mckeller <msmckeller@realequitymanagement.com>
2017-10-19 10:48:50 -0500

    Remove admin specific product service. Need to figure out a better way to handle this.

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {ProductService} from "@app/admin/services/product.service";
+import {ProductService} from "@services/product.service";
 import {ManageProductsModule} from "@app/admin/manage-products/manage-products.module";
 import { AdminComponent } from './admin.component';
 
