ac1229dc5d01504fa3ea67a3479dbf14fe9b3def
Matt Mckeller <msmckeller@realequitymanagement.com>
2017-10-19 10:49:31 -0500

    Linked model with the route params.

--- src/app/admin/manage-products/create-product/create-product.component.ts
+++ src/app/admin/manage-products/create-product/create-product.component.ts
@@ -4,7 +4,10 @@ import {Product} from "@models/product";
 import {CustomRegularExpressions} from "@models/custom-regular-expressions";
 import {FileUploader} from "ng2-file-upload";
 import {ProductImage} from "@models/product-image";
-import {ProductService} from "@app/admin/services/product.service";
+import {ProductService} from "@services/product.service";
+import {ActivatedRoute, ParamMap} from "@angular/router";
+
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'app-create-product',
@@ -22,7 +25,8 @@ export class CreateProductComponent implements OnInit {
 
   constructor(
     private formBuilder: FormBuilder,
-    private productService: ProductService
+    private productService: ProductService,
+    private route: ActivatedRoute
   ) {
     const _thisRef = this;
     this.form = formBuilder.group({
@@ -41,7 +45,11 @@ export class CreateProductComponent implements OnInit {
 
   submit() {
     console.log('Should submit product');
-    this.productService.create(this.model);
+    if(this.model.id) {
+      this.productService.update(this.model);
+    }else{
+      this.productService.create(this.model);
+    }
   }
 
   public fileOverBase(e: any): void {
@@ -51,6 +59,9 @@ export class CreateProductComponent implements OnInit {
   ngOnInit() {
     if (this.model === undefined) {
       this.model = new Product();
+      this.route.paramMap
+        .switchMap((params: ParamMap) => this.productService.getById(+params.get('id')))
+        .subscribe(product => this.model = product);
     }
 
     // Create product images form control separately since it's not a standard form element,
