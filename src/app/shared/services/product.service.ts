1a3263adc8192f8854ca1c994cac0af14e58fbbe
Matt Mckeller <msmckeller@realequitymanagement.com>
2017-10-19 10:46:13 -0500

    Clean up api url reference. Pull out next handling and error handling from individual methods. Add an update product method.

--- src/app/shared/services/product.service.ts
+++ src/app/shared/services/product.service.ts
@@ -13,18 +13,18 @@ const products: Product[] = [
 ];
 @Injectable()
 export class ProductService {
-
+  apiBaseUrl = 'http://api.expanseservices.com/api/';
   constructor(
     private http: HttpClient
   ) { }
 
   get(): Observable<Product[]> {
-    let url = 'http://api.expanseservices.com/api/products/getProducts';
+    let url = this.apiBaseUrl + 'products/getProducts';
     return this.http.get<Product[]>(url);
   }
 
   getById(id: any): Observable<Product> {
-    let url = 'http://api.expanseservices.com/api/products/getProduct/' + id;
+    let url = this.apiBaseUrl + 'products/getProduct/' + id;
     return this.http.get<Product>(url);
   }
 
@@ -32,11 +32,35 @@ export class ProductService {
 
   }
 
-  formatPrice(product: Product){
-    if (product && product.price) {
-      return '$' + product.price.toFixed(2);
-    }else {
-      return '';
+  create(productData: Product) {
+    let url = this.apiBaseUrl + 'products/createProduct';
+    this.http.post(url, {product: productData})
+      .subscribe(
+        this.nextHandler,
+        this.errorHandler
+      );
+  }
+
+  update(productData: Product) {
+    let url = this.apiBaseUrl + 'products/updateProduct';
+    this.http.post(url, {product: productData})
+      .subscribe(
+        this.nextHandler,
+        this.errorHandler
+      );
+  }
+
+  nextHandler(successData) {
+    console.log(successData);
+  }
+
+  errorHandler(err: HttpErrorResponse) {
+    if (err.error instanceof Error) {
+      // A client-side or network error occurred.
+      console.log('An error occurred:', err.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      console.log(`Backend returned code ${err.status}, body was: ${err.error}`);
     }
   }
 }
