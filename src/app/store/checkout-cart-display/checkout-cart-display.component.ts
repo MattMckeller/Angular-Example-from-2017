3580c1b16fa01f175a5e030d13cb0bd4cc903700
Matt Mckeller <msmckeller@realequitymanagement.com>
2017-10-24 13:21:56 -0500

    Cleanup checkout display - Now showing image icon, remove md list due to display issues, center elements in their containers.

--- src/app/store/checkout-cart-display/checkout-cart-display.component.ts
+++ src/app/store/checkout-cart-display/checkout-cart-display.component.ts
@@ -3,6 +3,9 @@ import { CartService } from '@app/shared/services/cart.service';
 import { ProductService } from '@app/shared/services/product.service';
 import { Product } from '@app/shared/models/product';
 import { Cart } from '@app/shared/models/cart';
+import {ProductImageService} from "@app/shared/services/product-image.service";
+import _ from "lodash";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'checkout-cart-display',
@@ -14,9 +17,12 @@ export class CheckoutCartDisplayComponent implements OnInit {
   @Input() displayTitle: boolean = true;
   cart: Cart;
   itemsBeingPurchased: Product[];
+  thumbnails: Array<any> = [];
 
   constructor(
     private productService: ProductService,
+    private router: Router,
+    private productImageService: ProductImageService,
     private cartService: CartService
   ) { }
 
@@ -30,7 +36,43 @@ export class CheckoutCartDisplayComponent implements OnInit {
   ngOnInit() {
     this.itemsBeingPurchased = [];
     this.cart = this.cartService.get();
+    this.fetchThumbnails();
     this.cart.items.forEach(item => this.itemsBeingPurchased.push(item));
+  }
+
+  /**
+   * Retrieve product iamge hrefs from api. Stores urls in thumbnail variable.
+   * todo: these methods are duplicated between components, clean them up.
+   */
+  fetchThumbnails(){
+    console.log('here');
+    let _this = this;
+    _.each(this.cart.items, function(product){
+      _this.productImageService.getProductImages(product).then(
+        (productImageHrefs) => {
+          console.log(productImageHrefs);
+          _this.thumbnails[product.id] = _this.thumbnails[product.id] || [];
+          productImageHrefs.forEach((imageHref) => _this.thumbnails[product.id].push(imageHref));
+        });
+    });
+  }
+
+  /**
+   * Returns the thumbnail for the provided product
+   * @param product
+   * @return false | href
+   */
+  getThumbnail(product) {
+    if(product && this.thumbnails[product.id] && this.thumbnails[product.id][0]){
+      return this.thumbnails[product.id][0];
+    }else{
+      return false;
+    }
+  }
+
+  redirect(productID){
+    console.log('do redirect for '+productID);
+    this.router.navigate(['/product',productID]);
   }
 
   get itemsInCartCount(){
