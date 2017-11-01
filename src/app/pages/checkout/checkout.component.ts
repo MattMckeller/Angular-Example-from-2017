3547b2559a670797443c6025f8c45227c0ced74a
Matt Mckeller <msmckeller@realequitymanagement.com>
2017-11-01 15:27:44 -0500

    Go back if there are no items in the users cart.

--- src/app/pages/checkout/checkout.component.ts
+++ src/app/pages/checkout/checkout.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 import { CartService } from '@services/cart.service';
 import { ProductService } from '@services/product.service';
 import { Product } from '@models/product';
 import { Cart } from '@models/cart';
-import {Router} from "@angular/router";
+import {Router} from '@angular/router';
+import {MatSnackBar} from '@angular/material';
 
 // todo create checkout module
 @Component({
@@ -18,16 +20,29 @@ export class CheckoutComponent implements OnInit {
   constructor(
     private productService: ProductService,
     private cartService: CartService,
-    private router: Router
+    private router: Router,
+    public snackBar: MatSnackBar,
+    private location: Location
   ) { }
 
   /**
    * Initialize the cart. Default to having all items in the cart being purchased.
    */
   ngOnInit() {
+    this.checkIfEmpty()
     this.itemsBeingPurchased = [];
     this.cart = this.cartService.get();
     this.cart.items.forEach(item => this.itemsBeingPurchased.push(item));
+  }
+
+  checkIfEmpty() {
+    if (this.cartService.isEmpty()) {
+      this.snackBar.open('There are no items in your cart.', 'Ok', {
+        duration: 5000,
+        verticalPosition: 'top'
+      });
+      this.location.back();
+    }
   }
 
 }
