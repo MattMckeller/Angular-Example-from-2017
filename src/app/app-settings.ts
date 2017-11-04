92981374ae6759dadb5686dbe5513b13842e144b
mattmckeller <mattmckeller@gmail.com>
2017-11-03 20:14:36 -0500

    spent hours trying to figure out htaccess problem due to ssl self signed issue to save 70$, shoulda spent 70$ especially since ITS STILL NOT QUITE RIGHT. it does work though...

--- src/app/app-settings.ts
+++ src/app/app-settings.ts
@@ -1,5 +1,5 @@
 export class AppSettings {
-  public static API_BASEURL = 'https://api.expanseservices.com/api/';
-  public static IMAGES_BASEURL = 'https://api.expanseservices.com/';
+  public static API_BASEURL = 'https://vintagehails.com/api/public/index.php/api/';
+  public static IMAGES_BASEURL = 'https://vintagehails.com/api/public/';
 
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   stripeKey: 'pk_live_G102Ygte2xoqV8t5MOubtXIH',
-  API_BASEURL: 'https://api.expanseservices.com/api/',
-  IMAGES_BASEURL: 'https://api.expanseservices.com/',
+  API_BASEURL: 'https://vintagehails.com/api/public/index.php/api/',
+  IMAGES_BASEURL: 'https://vintagehails.com/api/public/storage/'
 
 };
