37f275bd38ec88a7873624f29b17217761a735be
Matt Mckeller <mattmckeller@gmail.com>
2017-11-04 13:30:24 -0500

    Add holiday product images to homepage

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-import { ProductImage } from '@shared/models/product-image';
+import {ProductService} from "@app/shared/services/product.service";
+import {Product} from "@app/shared/models/product";
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-    productImages: Array<ProductImage>;
+    products: Array<Product>;
 
+    constructor(
+      private productService: ProductService
+    ){
+
+    }
     ngOnInit() {
+      this.products = [];
+      this.fetchFrontPageProducts();
+    }
+
+  fetchFrontPageProducts() {
+        this.productService.getById(13).subscribe(
+          (product)=>this.products.push(product)
+        );
+        this.productService.getById(14).subscribe(
+          (product)=>this.products.push(product)
+        );
     }
 
 
