92dba319598f149799b285255166e68ce58c98f7
mattmckeller <mattmckeller@gmail.com>
2017-11-04 13:12:45 -0500

    Changing machines

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ProductImage } from '@shared/models/product-image';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+    productImages: Array<ProductImage>;
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+
 
 }
