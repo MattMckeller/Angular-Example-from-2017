096998c1a115dde937d4ebf7caf119aeada83e01
Matt Mckeller <MattMckeller@gmail.com>
2020-01-20 11:59:54 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  stripeKey: 'pk_live_G102Ygte2xoqV8t5MOubtXIH',
+  stripeKey: 'abc123',
   API_BASEURL: 'https://vintagehails.com/api/public/index.php/api/',
   IMAGES_BASEURL: 'https://vintagehails.com/api/public/storage/'
 
